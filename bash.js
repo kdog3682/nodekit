55ad6ad28f31faa8b440ebbb44849cad671f0411
kdog3682 <kdog3682@gmail.com>
2024-03-14 23:58:32 -0400

    first commit

---
+++ bash.js
@@ -0,0 +1,23 @@
+// import * as chalk from "./chalk.js"
+import { exec } from "child_process"
+export { bash }
+
+async function bash(command) {
+    console.log(chalk.blue(command))
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                return reject(error)
+            }
+            const r = /^To github.com/i
+            if (r.test(stderr)) {
+                stdout += stderr
+                stderr = ''
+            }
+            if (stderr) {
+                return reject(stderr)
+            }
+            resolve(stdout)
+        })
+    })
+}
